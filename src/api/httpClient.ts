574143e29beffd001f6bcd094d208f31fc302c17
Andrii Lukianenko <andrii.lukianenko@yalantis.net>
2021-12-02 10:12:49 +0200

    hide `.env` and use `.env.example`

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: "https://api.polygon.io/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   // method: "GET",
   timeout: 4000,
   headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` },
